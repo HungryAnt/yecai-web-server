ca192205c549fca3192e5c54eeb3c6193afa2e49
HungryAnt <ljsunlin@126.com>
2015-09-17 22:53:11 +0800

    bug fix

--- src/main/java/com/antsoft/yecai/service/UserVehicleService.java
+++ src/main/java/com/antsoft/yecai/service/UserVehicleService.java
@@ -40,7 +40,7 @@ public class UserVehicleService {
     private String randomVehicle() {
         int[] nums = {39, 40, 50, 58, 59, 67, 74, 75, 81, 82, 83, 89, 90, 91};
         int index = random.nextInt(nums.length);
-        return String.format("vehicle_%s", index);
+        return String.format("vehicle_%s", nums[index]);
     }
 
     public void create(String userId, String vehicle) {
